973cb0a1e161a0914dbb5cb85abf32ccc908ab00
kumarasit <asit.km@bigbasket.com>
2018-02-16 14:27:38 +0530

    Initial Commit

---
+++ AssetStorage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssetStorage
+//
+//  Created by Asit Kumar Mohanty on 16/02/18.
+//  Copyright © 2018 Asit Kumar Mohanty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
